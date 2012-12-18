53ac6a0cab12a667a2077c8a908003d5fce7b350
Przemek Mroczek <przemek@motoprogrammer-linux.(none)>
2012-12-18 12:29:44 +0100

    rozwiazania do lab6

---
+++ lab6.sh
@@ -0,0 +1,15 @@
+# polecenie 1
+grep [[:alnum:]^] plik.txt
+
+# polecenie 2
+grep ^[0-9] pl*
+
+# polecenie 3
+grep "^.\{8\}r" plik.txt
+
+#polecenie 4
+grep -c ".*/bin/bash" /etc/passwd
+
+# polecenie 5
+grep [IVXLCDM] plik.txt
+
